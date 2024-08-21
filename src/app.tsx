42834b06a71ae07de493f133a3fa5476ed0522ba
Eduardo Tavares <edutav.dev@gmail.com>
2024-08-20 23:54:44 -0300

    reset do formulario

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
-import { Router } from './router'
-import { GlobalStyles } from './styles/global'
+import { CyclesContextProvider } from './contexts/CyclesContext'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
 
-      <GlobalStyles />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
